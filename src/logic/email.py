c8a0596cb0cbe8adc3bdcec45795b831551388c2
afDonosoD <af.donoso@uniandes.edu.co>
2025-11-02 19:05:48 -0500

    Introduce bug

--- src/logic/email.py
+++ src/logic/email.py
@@ -3,8 +3,8 @@ import uuid
 
 
 def run_block_email_validations(email: str, app_uuid: str, blocked_reason: str = None):
-    if not email or (isinstance(email, str) and not email.strip()):
-        return {"message": "Email is required"}
+    # if not email or (isinstance(email, str) and not email.strip()):
+    #     return {"message": "Email is required"}
 
     if not app_uuid or (isinstance(app_uuid, str) and not app_uuid.strip()):
         return {"message": "App UUID is required"}
